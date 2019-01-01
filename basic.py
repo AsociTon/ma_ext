d8b7605bf0a81a096600af11053b1b32d0b3e760
AsociTon <asociton@outlook.com>
2019-01-01 18:16:15 +0530

    Added plan and basic idea behind ma_ext
    This project is kept in the cool bag because of the absence  of time needed to  work on the  terms  to make even a basic model of the project.

--- basic.py
+++ basic.py
@@ -1,1 +1,29 @@
-print("Hello World")
+"""implement  a map and store all it's coordinates in a dbms over a place which is accessible via android kotlin '
+    dbms consisits of basically 3 coords 1) lat - long  and key
+
+
+    now we need to target and store all routes over the map
+
+    initialise the location of hospital over the map
+
+    now move the amb to the hospital
+
+    while not reached change all the keys around it's 250 m to red keys so they shift lanes
+
+    all other people using the navigation system get to know when they are around it with a flash over their screen
+
+"""
+
+'''Backend looks like #Server 
+
+fully coded in python may or may not display anything
+
+basic functionality :
+                  keep refreshing all the points over the dbms to servers 
+                  
+                  as soon as the ambulance design                 
+'''
+
+initiate = int(0)
+
+
